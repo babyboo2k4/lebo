218cdbae977dd190dc83f74596751969520c8541
babyboo2k4 <xnboo6124@gmail.com>
2023-09-12 20:28:41 +0700

    Exercise: Print hello world and calculate sum

---
+++ helloworld/helloworld.c
@@ -0,0 +1,8 @@
+//
+// Created by Admin on 9/11/2023.
+//
+#include <stdio.h>
+int main() {
+    printf("hello world");
+    return 0;
+}
---
+++ helloworld/summer.c
@@ -0,0 +1,14 @@
+//
+// Created by Admin on 9/11/2023.
+//
+#include <stdio.h>
+int main() {
+    int a, b, sum;
+    printf("Enter the first number: ");
+    scanf("%d",&a);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("the sum is: %d", sum);
+    return 0;
+}
